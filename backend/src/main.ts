63a4105662daf1af05061371117e3617a0fc2bac
Marc Dwyer <marcdwyer1738@gmail.com>
2020-02-07 01:32:07 -0600

    socketio is now connected to redis, this allows for much easier horizonatl scaling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { setWsHandlers } from "./ws_handler";
 import { createClient } from "redis";
 import io from "socket.io";
+import redisAdapter from "socket.io-redis";
 
 const RedisConfig = {
   port: 4200,
@@ -10,6 +11,7 @@ const RedisConfig = {
 
 async function main() {
   const wss = io(5000);
+  wss.adapter(redisAdapter(RedisConfig));
   const client = createClient(RedisConfig);
   setWsHandlers(wss, client);
 }
